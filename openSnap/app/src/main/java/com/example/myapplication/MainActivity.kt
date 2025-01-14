feb2cdee87f5e5f89a5047f919ec052f485b337d
Ville-Veikka Kiiskilä <villeveikkak@gmail.com>
2025-01-14 20:31:36 +0200

    HW2 navigation

--- openSnap/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ openSnap/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
@@ -23,17 +26,24 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Surface
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -72,29 +82,45 @@ class MainActivity : ComponentActivity() {
                     bottomBar = {
                         if (currentRoute !in hiddenBottomBar) {
                             BottomAppBar(
-                                modifier = Modifier.height(24.dp),
+                                modifier = Modifier.height(48.dp),
                                 content = {
-                                    Text(
-                                        text = "Bottom Bar",
-                                        modifier = Modifier
-                                            .padding(horizontal = 8.dp),
-                                        color = MaterialTheme.colorScheme.onPrimary
-                                    )
+                                    Row (horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()){
+                                        IconButton(onClick = { navController.navigate("messages_screen")}, modifier = Modifier.padding(end = 28.dp) ) {
+                                            Icon(imageVector = Icons.Default.Face, contentDescription = "Go to Messages", modifier = Modifier.size(36.dp))
+                                        }
+                                        IconButton(onClick = {
+                                            navController.navigate("home_screen") {
+                                                popUpTo("home_screen") { inclusive = true }
+                                            }
+                                        }, modifier = Modifier.padding(end = 28.dp)) {
+                                            Icon(imageVector = Icons.Default.Home, contentDescription = "Go to Home", modifier = Modifier.size(36.dp))
+                                        }
+                                        IconButton(onClick = { navController.navigate("settings_screen")}, modifier = Modifier.padding(end = 28.dp)) {
+                                            Icon(imageVector = Icons.Default.Settings, contentDescription = "Go to Settings", modifier = Modifier.size(36.dp))
+                                        }
+                                    }
                                 }
                             )
                         }
                     }
                 ) { innerPadding ->
+                    // At least 2 distinct views that can be moved between using buttons you created (5p)
                     NavHost(
                         navController = navController,
                         startDestination = "login_screen",
                         modifier = Modifier.padding(innerPadding)
                     ) {
                         composable("login_screen") {
-                            loginScreen()
+                            loginScreen(navController)
+                        }
+                        composable("home_screen") {
+                            homeScreen(navController)
                         }
                         composable("messages_screen") {
-                            messageScreen(isDarkTheme, sampleMessages)
+                            messageScreen(navController, isDarkTheme, sampleMessages)
+                        }
+                        composable("settings_screen") {
+                            settingsScreen(navController)
                         }
                     }
                 }
@@ -104,12 +130,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun loginScreen() {
-    Text("Hello")
+fun loginScreen(navController: NavController) {
+    // but circular navigation should be prevented! (5p)
+    // https://foso.github.io/Jetpack-Compose-Playground/activity/backhandler/
+    // https://stackoverflow.com/questions/67401294/jetpack-compose-close-application-by-button
+    var backPressedTime by remember { mutableLongStateOf(0) }
+    val activity = (LocalContext.current as? Activity)
+
+    BackHandler {
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - backPressedTime < 2000) {
+            Log.d("BackPress2", "Exiting activity, currentTime: $currentTime, backPressedTime: $backPressedTime")
+            activity?.finish()
+        } else {
+            backPressedTime = currentTime
+            Log.d("BackPress1", "Exiting activity, currentTime: $currentTime, backPressedTime: $backPressedTime")
+        }
+    }
+    Column {
+        Text("Hello login screen")
+        Button(
+            onClick = {
+                navController.navigate("home_screen") {
+                    popUpTo(navController.graph.startDestinationId) { inclusive = true }
+                    launchSingleTop = true
+                }
+            }
+        ) {
+            Text("Login")
+        }
+    }
 }
 
 @Composable
-fun messageScreen(isDarkTheme: MutableState<Boolean>, sampleMessages: List<Message>) {
+fun homeScreen(navController: NavController) {
+    Column {
+        Text("Hello home screen")
+    }
+}
+
+@Composable
+fun settingsScreen(navController: NavController) {
+    Column {
+        Text("Hello settings")
+        // but circular navigation should be prevented! (5p)
+        // https://stackoverflow.com/questions/71789903/does-navoptionsbuilder-launchsingletop-work-with-nested-navigation-graphs-in-jet
+        Button(onClick = {
+            navController.navigate("login_screen") {
+                popUpTo(navController.graph.startDestinationId) { inclusive = true }
+                launchSingleTop = true
+            }
+        }) {
+            Text("Sign out")
+        }
+    }
+}
+
+@Composable
+fun messageScreen(navController: NavController, isDarkTheme: MutableState<Boolean>, sampleMessages: List<Message>) {
     Column {
         // At least 2 different styles of text (0.5p)
         Row {
@@ -172,38 +250,6 @@ fun MessageList(messages: List<Message>, modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier.padding(all = 2.dp)) {
         items(messages) { message ->
             MessageCard(msg = message, modifier = Modifier.padding(bottom = 8.dp))
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewMessageList() {
-    val sampleMessages = listOf(
-        Message("Alice", "Hello! How are you?"),
-        Message("Bob", "I'm fine, thanks! What about you?"),
-        Message("Charlie", "Jetpack Compose is awesome!"),
-        Message("Daisy", "Don't forget to check the documentation.")
-    )
-
-    ComposeTutorialTheme() {
-        Surface {
-            MessageList(messages = sampleMessages)
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewMessageCard() {
-    ComposeTutorialTheme() {
-        Surface {
-            MessageCard(
-                msg = Message(
-                    author = "John Doe",
-                    body = "This is a sample message to preview the MessageCard."
-                )
-            )
         }
     }
 }
