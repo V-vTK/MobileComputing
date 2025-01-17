772f789c53000da68409d4f15888df6598ce5317
Ville-Veikka Kiiskilä <villeveikkak@gmail.com>
2025-01-17 20:44:38 +0200

    Squashed and merged changes from HW3

--- openSnap/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ openSnap/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.myapplication
 
-import android.app.Activity
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
@@ -14,7 +13,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.ComposeTutorialTheme
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -24,9 +22,9 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Surface
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -39,19 +37,31 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberAsyncImagePainter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
 
 class MainActivity : ComponentActivity() {
+    private lateinit var dataStoreManager: DataStoreManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
+        dataStoreManager = DataStoreManager(this)
 
         val sampleMessages = listOf(
             Message("Alice", "Hello!"),
@@ -75,7 +85,7 @@ class MainActivity : ComponentActivity() {
             val isDarkTheme = remember { mutableStateOf(true) }
             val navController = rememberNavController()
             val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
-            val hiddenBottomBar: List<String> = listOf("login_screen")
+            val hiddenBottomBar: List<String> = listOf("login_screen", "register_screen")
 
             ComposeTutorialTheme(darkTheme = isDarkTheme.value) {
                 Scaffold(
@@ -113,6 +123,9 @@ class MainActivity : ComponentActivity() {
                         composable("login_screen") {
                             loginScreen(navController)
                         }
+                        composable("register_screen") {
+                            registerScreen(navController, dataStoreManager)
+                        }
                         composable("home_screen") {
                             homeScreen(navController)
                         }
@@ -134,19 +147,8 @@ fun loginScreen(navController: NavController) {
     // but circular navigation should be prevented! (5p)
     // https://foso.github.io/Jetpack-Compose-Playground/activity/backhandler/
     // https://stackoverflow.com/questions/67401294/jetpack-compose-close-application-by-button
-    var backPressedTime by remember { mutableLongStateOf(0) }
-    val activity = (LocalContext.current as? Activity)
+    BackPressHandler()
 
-    BackHandler {
-        val currentTime = System.currentTimeMillis()
-        if (currentTime - backPressedTime < 2000) {
-            Log.d("BackPress2", "Exiting activity, currentTime: $currentTime, backPressedTime: $backPressedTime")
-            activity?.finish()
-        } else {
-            backPressedTime = currentTime
-            Log.d("BackPress1", "Exiting activity, currentTime: $currentTime, backPressedTime: $backPressedTime")
-        }
-    }
     Column {
         Text("Hello login screen")
         Button(
@@ -159,8 +161,80 @@ fun loginScreen(navController: NavController) {
         ) {
             Text("Login")
         }
+        Button(
+            onClick = {
+                navController.navigate("register_screen") {
+                    popUpTo(navController.graph.startDestinationId) { inclusive = true }
+                    launchSingleTop = true
+                }
+            }
+        ) {
+            Text("Sign up instead!")
+        }
     }
 }
+
+@Composable
+fun registerScreen(navController: NavController, dataStoreManager: DataStoreManager) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val selectedImageUri = remember { mutableStateOf<Uri?>(null) }
+    val selectedImageVal = remember { mutableStateOf<String?>(null) }
+    val context = LocalContext.current
+    val dataStoreManager = DataStoreManager(context)
+
+    BackPressHandler()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Hello Register Screen", modifier = Modifier.padding(bottom = 16.dp, top= 16.dp))
+        TextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Enter your username") },
+            modifier = Modifier.fillMaxWidth(0.6f)
+        )
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Enter your password") },
+            modifier = Modifier.fillMaxWidth(0.6f)
+        )
+        // Input and display at least some text and a picture in one view (5p)
+        // https://stackoverflow.com/questions/78110240/how-to-pass-a-value-from-a-composable-function-to-another-composable-function-an
+        imagePicker(selectedImageUri = selectedImageUri.value,
+            onImageSelected = { uri ->
+                Log.d("ImagePickerExample", "Selected Image URI: $uri , $selectedImageUri")
+                val imagePath = saveImageToLocalStorage(uri, context)
+                selectedImageUri.value = Uri.parse(imagePath)
+                selectedImageVal.value = imagePath
+                Log.d("ImagePickerExample", "Saved Image Path: $imagePath")
+            }
+        )
+        // Display given text and picture in another view and retain these changes when restarting application (5p)
+        Button(onClick = {
+            val user = userStore(
+                username = username,
+                password = password,
+                profile_picture = selectedImageVal.value ?: ""
+            )
+
+            CoroutineScope(Dispatchers.IO).launch {
+                dataStoreManager.saveToDataStore(user)
+            }
+
+            navController.navigate("home_screen")
+        }) {
+            Text("Submit")
+        }
+    }
+}
+
 
 @Composable
 fun homeScreen(navController: NavController) {
@@ -171,17 +245,57 @@ fun homeScreen(navController: NavController) {
 
 @Composable
 fun settingsScreen(navController: NavController) {
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    val dataStoreManager = DataStoreManager(context)
+    val username = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+    val profilePicture = remember { mutableStateOf("") }
+
+    // https://stackoverflow.com/questions/70480709/what-is-the-useeffect-correspondent-in-android-compose-component
+    LaunchedEffect(Unit) {
+        dataStoreManager.getFromDataStore().collect { userStore ->
+            username.value = userStore.username
+            password.value = userStore.password
+            profilePicture.value = userStore.profile_picture
+        }
+    }
     Column {
         Text("Hello settings")
         // but circular navigation should be prevented! (5p)
         // https://stackoverflow.com/questions/71789903/does-navoptionsbuilder-launchsingletop-work-with-nested-navigation-graphs-in-jet
         Button(onClick = {
-            navController.navigate("login_screen") {
-                popUpTo(navController.graph.startDestinationId) { inclusive = true }
-                launchSingleTop = true
+            coroutineScope.launch {
+                navController.navigate("login_screen") {
+                    popUpTo(navController.graph.startDestinationId) { inclusive = true }
+                    launchSingleTop = true
+                }
+                dataStoreManager.clearDataStore()
+                dataStoreManager.getFromDataStore().collect { userStore ->
+                    username.value = userStore.username
+                    password.value = userStore.password
+                    profilePicture.value = userStore.profile_picture
+                }
+                Log.d("username AFTER", username.value)
             }
         }) {
             Text("Sign out")
+        }
+
+        Text("username: ${username.value}")
+        Text("password: ${password.value}")
+        Text("Profile_picture: ${profilePicture.value}")
+        if (profilePicture.value.isNotEmpty()) {
+            val file = File(profilePicture.value)
+            if (file.exists()) {
+                Image(
+                    painter = rememberAsyncImagePainter(file),
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier.fillMaxWidth()
+                )
+            } else {
+                Text("Profile picture not found.")
+            }
         }
     }
 }
@@ -250,38 +364,6 @@ fun MessageList(messages: List<Message>, modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier.padding(all = 2.dp)) {
         items(messages) { message ->
             MessageCard(msg = message, modifier = Modifier.padding(bottom = 8.dp))
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewMessageList() {
-    val sampleMessages = listOf(
-        Message("Alice", "Hello! How are you?"),
-        Message("Bob", "I'm fine, thanks! What about you?"),
-        Message("Charlie", "Jetpack Compose is awesome!"),
-        Message("Daisy", "Don't forget to check the documentation.")
-    )
-
-    ComposeTutorialTheme() {
-        Surface {
-            MessageList(messages = sampleMessages)
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewMessageCard() {
-    ComposeTutorialTheme() {
-        Surface {
-            MessageCard(
-                msg = Message(
-                    author = "John Doe",
-                    body = "This is a sample message to preview the MessageCard."
-                )
-            )
         }
     }
 }
---
+++ openSnap/app/src/main/java/com/example/myapplication/utility.kt
@@ -0,0 +1,95 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import java.io.File
+import java.io.FileOutputStream
+
+fun handleBackPress(activity: Activity?, backPressedTime: Long, onBackPressedTimeUpdated: (Long) -> Unit) {
+    val currentTime = System.currentTimeMillis()
+    if (currentTime - backPressedTime < 500) {
+        Log.d("BackPress2", "Exiting activity, currentTime: $currentTime, backPressedTime: $backPressedTime")
+        activity?.finish()
+    } else {
+        onBackPressedTimeUpdated(currentTime)
+        Log.d("BackPress1", "Exiting activity, currentTime: $currentTime, backPressedTime: $backPressedTime")
+    }
+}
+
+@Composable
+fun BackPressHandler() {
+    var backPressedTime by remember { mutableLongStateOf(0) }
+    val activity = (LocalContext.current as? Activity)
+
+    BackHandler {
+        handleBackPress(activity, backPressedTime) { newBackPressedTime ->
+            backPressedTime = newBackPressedTime
+        }
+    }
+}
+
+@Composable
+fun imagePicker(selectedImageUri: Uri?, onImageSelected: (Uri?) -> Unit) {
+    // https://medium.com/@yogesh_shinde/implementing-image-video-documents-picker-in-jetpack-compose-73ef846cfffb
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        onImageSelected(uri)
+    }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,) {
+        Button(onClick = {
+            launcher.launch(
+                PickVisualMediaRequest(mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly)
+            )
+        }) {
+            Text(text = "Select Image")
+        }
+
+        selectedImageUri?.let { image ->
+            val painter = rememberAsyncImagePainter(
+                ImageRequest
+                    .Builder(LocalContext.current)
+                    .data(data = image)
+                    .build()
+            )
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(250.dp, 250.dp)
+                    .padding(16.dp)
+            )
+        }
+    }
+}
+
+fun saveImageToLocalStorage(uri: Uri?, context: Context): String? {
+    val inputStream = uri?.let { context.contentResolver.openInputStream(it) }
+    val file = File(context.filesDir, "profile_image.jpg")
+    val outputStream = FileOutputStream(file)
+    inputStream?.copyTo(outputStream)
+
+    return file.absolutePath
+}
